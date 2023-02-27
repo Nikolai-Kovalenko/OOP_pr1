49358a763570a645796b28e3c2e1d5fe0f6a3c4f
NickK <nikolay.Kov.Al.006@gmail.com>
2023-02-28 01:08:05 +0200

    add ex1

--- pr1_ex1/Program.cs
+++ pr1_ex1/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
-namespace MyFirstLambda
-{
-   class Main
-   {
-       
-   }
-}
+      void Add(int x, int y) => Console.WriteLine(x + y);
+
+      Add(10, 4);
+
+
+      void Sub(int x, int y) => Console.WriteLine(x - y);
+
+      Sub(10, 4);
+
+      void Mul(int x, int y) => Console.WriteLine(x * y);
+
+      Mul(10, 4);
+
+
+      void Div(int x, int y) => Console.WriteLine(y != 0 ?  x / y : 0);
+
+      Div(10, 0);
+
