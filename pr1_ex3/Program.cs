0a93b42926a29fad7b86612475ed72df9864d425
NickK <nikolay.Kov.Al.006@gmail.com>
2023-03-05 02:10:51 +0200

    v 3.01

--- pr1_ex3/Program.cs
+++ pr1_ex3/Program.cs
@@ -1,3 +1,3 @@
-﻿Func<int, int, int, float> avg = (int x, int y, int z) => (x + y + z) / 3; 
+﻿Func<int, int, int, double> avg = (int x, int y, int z) => (x + y + z) / 3.0; 
 
 System.Console.WriteLine(avg(1, 1, 5));
